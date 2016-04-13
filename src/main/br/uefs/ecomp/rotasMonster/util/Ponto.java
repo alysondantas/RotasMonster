e01965f6e520e522fdc7635a403c64fca1b423dd
alysondantas <alyson.lipe@gmail.com>
2016-04-13 20:22:05 -0300

    Criando classes

---
+++ src/main/br/uefs/ecomp/rotasMonster/util/Ponto.java
@@ -0,0 +1,5 @@
+package br.uefs.ecomp.rotasMonster.util;
+
+public class Ponto {
+	private Lista aresta;
+}
---
+++ src/test/br/uefs/ecomp/rotasMonster/controller/CadastroPontosTest.java
@@ -0,0 +1,5 @@
+package br.uefs.ecomp.rotasMonster.controller;
+
+public class CadastroPontosTest {
+
+}
