c36d6d395fd789c0253c2eb4122853ece910b59e
alysondantas <alyson.lipe@gmail.com>
2016-04-15 00:24:21 -0300

    teste

--- src/test/br/uefs/ecomp/rotasMonster/Model/PontosTest.java
+++ src/test/br/uefs/ecomp/rotasMonster/Model/PontosTest.java
@@ -1,5 +1,32 @@
 package br.uefs.ecomp.rotasMonster.Model;
 
-public class PontosTest {
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+import br.uefs.ecomp.rotasMonster.controller.*;
+import br.uefs.ecomp.rotasMonster.exceptions.*;
+import br.uefs.ecomp.rotasMonster.util.*;
+
+public class PontosTest {
+	AdministradorController controller = new AdministradorController();
+	
+	@Test
+	public void cadastrarPontoSucesso(){
+		Ponto a = new Ponto("A", 1);
+		Ponto b = new Ponto("B", 1);
+		
+		try{
+			Ponto b1 = controller.cadastrarPonto(b);
+			Ponto a1 = controller.cadastrarPonto(a);
+		}catch(PontoNuloException e){
+			fail();
+		} catch (CampoObrigatorioInexistenteException e) {
+			fail();
+		} catch (ConflitoException e) {
+			fail();
+		}
+		
+		
+	}
 }
