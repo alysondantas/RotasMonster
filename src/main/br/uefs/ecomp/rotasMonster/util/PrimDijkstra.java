f8c98fe2da72beea61394dc56f6c8a7550d5d8fb
alysondantas <alyson.lipe@gmail.com>
2016-04-18 17:01:40 -0300

    Criando classes

---
+++ src/main/br/uefs/ecomp/rotasMonster/util/PrimDijkstra.java
@@ -0,0 +1,19 @@
+package br.uefs.ecomp.rotasMonster.util;
+
+import br.uefs.ecomp.rotasMonster.model.Grafo;
+
+public class PrimDijkstra {
+	private Grafo grafo;
+	
+	public PrimDijkstra(Grafo grafo){
+		this.grafo = grafo;
+	}
+	
+	public void rodarPrim(){
+		
+	}
+	
+	public void dijkstra(){
+		
+	}
+}
